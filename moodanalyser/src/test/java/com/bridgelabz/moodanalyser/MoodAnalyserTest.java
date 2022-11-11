23fb51099e8b9572ae370bfaf907c4fbe9cb6a03
PREETI2096 <preetirg96@gmail.com>
2022-11-11 22:48:46 +0530

    [preeti]:added Refactor

--- moodanalyser/src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
+++ moodanalyser/src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
@@ -4,10 +4,18 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MoodAnalyserTest {
+	
+	 @Test
+	    public void givenMessage_WhenProper_ShouldReturnSad(){
+	        MoodAnalyser moodAnalyser1 = new MoodAnalyser("I am in Sad mood.");
+	        String result = moodAnalyser1.analyseMood("Sad");
+	        System.out.println(result);
+	        Assertions.assertEquals("Sad" , result);
+	    }
 	 @Test
 	    public void givenMessage_WhenProper_ShouldReturnHappy(){
-	        MoodAnalyser moodAnalyser = new MoodAnalyser();
-	        String result = moodAnalyser.analyseMood("I am in a Happy Mood");
+	        MoodAnalyser moodAnalyser2 = new MoodAnalyser("Happy");
+	        String result = moodAnalyser2.analyseMood("I am in a Happy Mood");
 	        System.out.println(result);
 	        Assertions.assertEquals("Happy" , result);
 	    }
